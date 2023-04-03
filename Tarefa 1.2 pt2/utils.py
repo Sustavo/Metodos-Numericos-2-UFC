8b606c14b24c240704a9fc0ff4d6ea62cf058511
Gustavo <gust4v0309@gmail.com>
2023-04-03 19:00:32 -0300

    Feat: utils

---
+++ Tarefa 1.2 pt2/utils.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageFilter
+import os
+import numpy as np
+
+# Caminho relativo das pastas de entrada e saída de imagens
+INPUT_DIR = os.path.join('images', 'input')
+OUTPUT_DIR = os.path.join('images', 'output')
+
+
+def in_file(filename):
+    '''Retorna o caminho de um arquivo de entrada'''
+
+    return os.path.join(INPUT_DIR, filename)
+
+def out_file(filename):
+    '''Retorna o caminho de um arquivo de saída'''
+
+    return os.path.join(OUTPUT_DIR, filename)
+
